29bdfbe2ee3229550e443d10fb40b03290754488
FurkanMDemiray <furkanmdemiray@gmail.com>
2023-12-22 17:40:50 +0300

    added CryptoPresenter

--- CryptoViper/Presenter.swift
+++ CryptoViper/Presenter.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+
+protocol AnyPresenter {
+
+    var router: AnyRouter? { get set }
+    var interactor: AnyInteractor? { get set }
+    var view: AnyView? { get set }
+
+
+    func interactorDidDownloadCryptoData(result: Result<[Crypto], Error>)
+}
+
+class CryptoPresenter: AnyPresenter {
+
+    var interactor: AnyInteractor?
+    var view: AnyView?
+    var router: AnyRouter?
+
+    func interactorDidDownloadCryptoData(result: Result<[Crypto], Error>) {
+        switch result {
+        case .success(let cryptoList):
+            //view?.updateView(with: cryptoList))
+            print("Update")
+        case .failure(let error):
+            //view?.showError(error)
+            print("Error")
+        }
+    }
+}
